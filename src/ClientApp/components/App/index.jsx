58fc5ac0e809cc054ee6415f68b8e15395deed39
Alex768 <vortigont768@inbox.ru>
2019-06-28 19:00:17 +0500

    Refactoring

--- src/ClientApp/components/App/index.jsx
+++ src/ClientApp/components/App/index.jsx
@@ -5,18 +5,23 @@ import Modal from '@skbkontur/react-ui/Modal';
 import Button from '@skbkontur/react-ui/Button';
 
 export default class App extends React.Component {
+
     constructor() {
         super();
         this.state = {
             score: 50,
             map: null,
-            playerCoords: null,
-            id: 0,
-            modalVisible: false,
-            isFinished: false,
-            level: 0
+            level: 0,
+            levelId: 0,
+            modalVisible: false
         };
         this.keyListener = null;
+        this.movementDirections = {
+            37: 1,
+            38: 2,
+            39: 0,
+            40: 3
+        };
         this.wonMessage = {
             title: 'Поздравляем!',
             body: 'Вы успешно прошли уровень.'
@@ -25,51 +30,6 @@ export default class App extends React.Component {
 
     componentDidMount() {
         this.startLevel();
-    }
-
-    notifyMovement(direction) {
-        fetch('/api/game/' + this.state.id + '?movement=' + direction)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                return Promise.reject();
-            })
-            .then(data => {
-                const mapArr = data.map.split(' ').map((str, index) => str.split(','));
-                this.setState({
-                    map: mapArr,
-                    modalVisible: data.isFinished
-                });
-                data.inFinished && this.endLevel();
-            });
-    }
-
-    startLevel = () => {
-        console.dir(this.state.level > 0);
-        const URL = '/api/game' + (this.state.level === 0 ? '' : ('?level=' + this.state.level));
-        fetch(URL)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                return Promise.reject();
-            })
-            .then(data => {
-                const mapArr = data.map.split(' ').map((str) => str.split(','));
-                this.setState({
-                    map: mapArr,
-                    id: data.id,
-                    isFinished: data.isFinished,
-                    level: this.state.level + 1
-                });
-                return;
-            });
-        this.keyListener = document.addEventListener('keyup', this.keyUpEventHandler, false);
-    }
-
-    endLevel = () => {
-        document.removeEventListener(this.keyListener);
     }
 
     render() {
@@ -84,42 +44,98 @@ export default class App extends React.Component {
         );
     }
 
+    /**
+     * Выполняет запрос на сервер
+     * @param {String} url - адрес запроса
+     * @param {Function|undefined} callback - коллбэк
+     */
+    sendRequest = (url, callback) => {
+        fetch(url)
+            .then(response => response.ok ? response.json() : Promise.reject())
+            .then(data => { typeof callback === 'function' && callback(data) });
+    }
+
+    /**
+     * Начинает игру
+     * @returns {undefined}
+     */
+    startLevel = () => {
+        const URL = '/api/game' + (this.state.level === 0 ? '' : ('?level=' + this.state.level));
+        this.sendRequest(URL, (data) => {
+            const mapArr = data.map.split(' ').map((str, index) => str.split(','));
+            this.setState({
+                map: mapArr,
+                levelId: data.id,
+                modalVisible: data.isFinished,
+                level: this.state.level + 1
+            });
+            this.keyListener = document.addEventListener('keyup', this.keyboardListener, false);
+        });
+    }
+
+    /**
+     * Уведомляет сервер о переходе в указанном направлении
+     * @param {Number} direction - код направления из this.movementDirections
+     * @returns {undefined}
+     */
+    notifyMovement(direction) {
+        const URL = '/api/game/' + this.state.levelId + '?movement=' + direction;
+        this.sendRequest(URL, (data) => {
+            const mapArr = data.map.split(' ').map((str, index) => str.split(','));
+            data.inFinished && document.removeEventListener(this.keyListener);
+            this.setState({
+                map: mapArr,
+                modalVisible: data.isFinished
+            });
+        });
+    }
+
+    /**
+     * Выводит модальное окно с сообщением
+     * @param {string} title - текст заголовка
+     * @param {string|*} body - сообщение
+     * @returns {*}
+     */
     renderModal = ({title, body}) => {
-        const onClose = () => { this.switchModalVisiblility(); this.startLevel(); };
+        const onClose = () => {
+            this.switchModalVisiblility();
+            this.startLevel();
+        };
         return (
             <Modal onClose={onClose}>
                 <Modal.Header>{title}</Modal.Header>
-                <Modal.Body>
-                    {body}
-                </Modal.Body>
+                <Modal.Body>{body}</Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={onClose}>Продолжить</Button>
+                    <Button onClick={onClose} use="primary">Следующий уровень</Button>
                 </Modal.Footer>
             </Modal>
         );
-    };
+    }
 
+    /**
+     * Переключает отображение модального окна
+     * @returns {undefined}
+     */
     switchModalVisiblility = () => {
         this.setState({
             modalVisible: !this.state.modalVisible
         });
-    };
+    }
 
-    keyUpEventHandler = (event) => {
+    /**
+     * Прослушивает нажатие на клавиатуру
+     * @param {Object} event - событие
+     * @returns {undefined}
+     */
+    keyboardListener = (event) => {
         if (this.state.modalVisible) {
             return;
         }
-        const DIRECTIONS = {
-            37: 1,
-            38: 2,
-            39: 0,
-            40: 3
-        };
         const CODE = event.keyCode;
-        if (!DIRECTIONS.hasOwnProperty(CODE)) {
+        if (! this.movementDirections.hasOwnProperty(CODE)) {
             return;
         }
+        this.notifyMovement(this.movementDirections[CODE]);
+    }
 
-        this.notifyMovement(DIRECTIONS[CODE]);
-    };
 }
