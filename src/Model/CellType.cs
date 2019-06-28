0ed462b85de24358148815a869703c36a67d2894
user <user@skb.kontur.ru>
2019-06-28 10:41:59 +0500

    Add enum CellType.cs

---
+++ src/Model/CellType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thegame.Model
+{
+    public enum CellType
+    {
+        Empty,
+        Wall,
+        Warehouse,
+        Box,
+        Player
+    }
+
+    public enum Movement
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
